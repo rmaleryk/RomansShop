76cd5b86e1aa0f536efe136e965e9acaf68152cb
rmaleryk <seaker000@gmail.com>
2018-03-27 17:55:52 +0300

    Add Product reactive form was refactored

--- RomansShop.ClientApp/src/app/admin/products/edit-product/edit-product.component.ts
+++ RomansShop.ClientApp/src/app/admin/products/edit-product/edit-product.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit, Input, OnDestroy } from "@angular/core";
 import { Router, ActivatedRoute } from "@angular/router";
 import { FormGroup, FormBuilder, Validators, FormControl } from "@angular/forms";
-import { Subject } from "rxjs/Subject";
-import 'rxjs/add/operator/takeUntil';
 
 import { Product } from "../../../shared/models/product";
 import { ProductService } from "../../../api/product.service";
@@ -12,12 +10,11 @@ import { CategoryService } from "../../../api/category.service";
 @Component({
     templateUrl: './edit-product.component.html'
 })
-export class EditProductComponent implements OnInit, OnDestroy {
+export class EditProductComponent implements OnInit {
     categories: Category[];
     productId: string;
     isLoaded: boolean;
     productForm: FormGroup;
-    destroy$: Subject<boolean> = new Subject<boolean>();
 
     constructor(private productService: ProductService,
                 private categoryService: CategoryService,
@@ -29,16 +26,10 @@ export class EditProductComponent implements OnInit, OnDestroy {
     ngOnInit() {
         this.productId = this.activeRoute.snapshot.params["id"];
         
-        this.productForm = this.formBuilder.group({
-            "name": ["", [Validators.required, Validators.maxLength(30)]],
-            "categoryId": [""],
-            "price": ["", [Validators.required]],
-            "description": ["", [Validators.maxLength(255)]],
-        });
-
         if (this.productId != null) {
             this.loadProduct(this.productId);
         } else {
+            this.buildForm({});
             this.isLoaded = true;
         }
 
@@ -47,7 +38,6 @@ export class EditProductComponent implements OnInit, OnDestroy {
 
     private loadCategories() {
         this.categoryService.getCategories()
-            .takeUntil(this.destroy$)
             .subscribe(
                 (data: Category[]) => this.categories = data
             );
@@ -55,43 +45,36 @@ export class EditProductComponent implements OnInit, OnDestroy {
 
     private loadProduct(id: string) {
         this.productService.getById(id)
-            .takeUntil(this.destroy$)
             .subscribe(
                 (data: Product) => {
-                    this.productForm.controls["name"].setValue(data.name);
-                    this.productForm.controls["categoryId"].setValue(data.categoryId);
-                    this.productForm.controls["price"].setValue(data.price);
-                    this.productForm.controls["description"].setValue(data.description);
+                    this.buildForm(data);
                     this.isLoaded = true;
                 }
             );
     }
 
     private save() {
-        const product: Product = new Product({});
-        product.id = this.productId;
-        product.name = this.productForm.controls["name"].value;
-        product.categoryId = this.productForm.controls["categoryId"].value;
-        product.price = this.productForm.controls["price"].value;
-        product.description = this.productForm.controls["description"].value;
+        const product: Product = new Product({
+            id: this.productId,
+            name: this.productForm.controls["name"].value,
+            categoryId: this.productForm.controls["categoryId"].value,
+            price: this.productForm.controls["price"].value,
+            description: this.productForm.controls["description"].value
+        });
 
-        if (this.productId == null) {
-            this.productService.create(product)
-                .takeUntil(this.destroy$)
-                .subscribe(
-                    (data: any) => this.router.navigateByUrl("admin/products")
-                );
-        } else {
-            this.productService.update(product)
-                .takeUntil(this.destroy$)
-                .subscribe(
-                    (data: any) => this.router.navigateByUrl("admin/products")
-                );
-        }
+        (product.id == null ? this.productService.create(product) : this.productService.update(product))
+            .subscribe(
+                (data: any) => this.router.navigateByUrl("admin/products")
+            );
     }
 
-    ngOnDestroy() {
-        this.destroy$.next(true);
-        this.destroy$.unsubscribe();
+    private buildForm(product: any) {
+        this.productForm = this.formBuilder
+            .group({
+                "name": [product.name, [Validators.required, Validators.maxLength(30)]],
+                "categoryId": [product.categoryId],
+                "price": [product.price, [Validators.required]],
+                "description": [product.description, [Validators.maxLength(255)]]
+            });
     }
 }
